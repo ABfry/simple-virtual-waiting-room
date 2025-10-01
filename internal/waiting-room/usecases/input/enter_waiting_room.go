87ef478afc45b3aeede5b1e9c2ca85f53355b82f
ABfry <is0605ek@ed.ritsumei.ac.jp>
2025-10-01 23:39:03 +0900

    feat: 待機室入場処理の入力および出力構造体を追加

---
+++ internal/waiting-room/usecases/input/enter_waiting_room.go
@@ -0,0 +1,21 @@
+package input
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type EnterWaitingRoomInput struct {
+	WaitingRoomID uuid.UUID
+	UserID        uuid.UUID
+	UserName      *string
+	Now           time.Time
+}
+
+func (i EnterWaitingRoomInput) EffectiveTime() time.Time {
+	if i.Now.IsZero() {
+		return time.Now()
+	}
+	return i.Now
+}
